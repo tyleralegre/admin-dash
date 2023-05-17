63f28f191558127d759038bb833366af9135b840
Tyler <109047657+tyleralegre@users.noreply.github.com>
2023-05-16 20:33:15 -0700

    update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import {ContextProvider} from './contexts/ContextProvider';
+import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
-<ContextProvider>
-<App/>
-</ContextProvider>,
- document.getElementById('root')
- );
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
