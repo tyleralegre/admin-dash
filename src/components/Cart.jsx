5f54c1fc0e7ffc86cf8fd0009a70302c53639578
Tyler <109047657+tyleralegre@users.noreply.github.com>
2023-06-01 12:21:48 -0700

    update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
-
 import { useStateContext } from '../contexts/ContextProvider';
 import { cartData } from '../data/dummy';
 import { Button } from '.';
@@ -68,3 +67,4 @@ const Cart = () => {
 };
 
 export default Cart;
+.
